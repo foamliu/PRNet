85e3d6fce6cd16137583d588aec3e694e1dca581
Yang Liu <liuyang12@focusmedia.cn>
2019-12-11 15:24:15 +0800

    update

--- demo.py
+++ demo.py
@@ -1,19 +1,20 @@
-import numpy as np
+import argparse
+import ast
 import os
 from glob import glob
+from time import time
+
+import numpy as np
 import scipy.io as sio
 from skimage.io import imread, imsave
 from skimage.transform import rescale, resize
-from time import time
-import argparse
-import ast
 
 from api import PRN
-
 from utils.estimate_pose import estimate_pose
-from utils.rotate_vertices import frontalize
 from utils.render_app import get_visibility, get_uv_mask, get_depth_image
+from utils.rotate_vertices import frontalize
 from utils.write import write_obj_with_colors, write_obj_with_texture
+
 
 def main(args):
     if args.isShow or args.isTexture:
@@ -21,8 +22,8 @@ def main(args):
         from utils.cv_plot import plot_kpt, plot_vertices, plot_pose_box
 
     # ---- init PRN
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu # GPU number, -1 for CPU
-    prn = PRN(is_dlib = args.isDlib)
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu  # GPU number, -1 for CPU
+    prn = PRN(is_dlib=args.isDlib)
 
     # ------------- load data
     image_folder = args.inputDir
@@ -31,7 +32,7 @@ def main(args):
         os.mkdir(save_folder)
 
     types = ('*.jpg', '*.png')
-    image_path_list= []
+    image_path_list = []
     for files in types:
         image_path_list.extend(glob(os.path.join(image_folder, files)))
     total_num = len(image_path_list)
@@ -43,25 +44,25 @@ def main(args):
         # read image
         image = imread(image_path)
         [h, w, c] = image.shape
-        if c>3:
-            image = image[:,:,:3]
+        if c > 3:
+            image = image[:, :, :3]
 
         # the core: regress position map
         if args.isDlib:
             max_size = max(image.shape[0], image.shape[1])
-            if max_size> 1000:
-                image = rescale(image, 1000./max_size)
-                image = (image*255).astype(np.uint8)
-            pos = prn.process(image) # use dlib to detect face
+            if max_size > 1000:
+                image = rescale(image, 1000. / max_size)
+                image = (image * 255).astype(np.uint8)
+            pos = prn.process(image)  # use dlib to detect face
         else:
             if image.shape[0] == image.shape[1]:
-                image = resize(image, (256,256))
-                pos = prn.net_forward(image/255.) # input image has been cropped to 256x256
+                image = resize(image, (256, 256))
+                pos = prn.net_forward(image / 255.)  # input image has been cropped to 256x256
             else:
-                box = np.array([0, image.shape[1]-1, 0, image.shape[0]-1]) # cropped with bounding box
+                box = np.array([0, image.shape[1] - 1, 0, image.shape[0] - 1])  # cropped with bounding box
                 pos = prn.process(image, box)
-        
-        image = image/255.
+
+        image = image / 255.
         if pos is None:
             continue
 
@@ -72,7 +73,7 @@ def main(args):
                 save_vertices = frontalize(vertices)
             else:
                 save_vertices = vertices.copy()
-            save_vertices[:,1] = h - 1 - save_vertices[:,1]
+            save_vertices[:, 1] = h - 1 - save_vertices[:, 1]
 
         if args.isImage:
             imsave(os.path.join(save_folder, name + '.jpg'), image)
@@ -83,27 +84,31 @@ def main(args):
 
             if args.isTexture:
                 if args.texture_size != 256:
-                    pos_interpolated = resize(pos, (args.texture_size, args.texture_size), preserve_range = True)
+                    pos_interpolated = resize(pos, (args.texture_size, args.texture_size), preserve_range=True)
                 else:
                     pos_interpolated = pos.copy()
-                texture = cv2.remap(image, pos_interpolated[:,:,:2].astype(np.float32), None, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT,borderValue=(0))
+                texture = cv2.remap(image, pos_interpolated[:, :, :2].astype(np.float32), None,
+                                    interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
                 if args.isMask:
                     vertices_vis = get_visibility(vertices, prn.triangles, h, w)
                     uv_mask = get_uv_mask(vertices_vis, prn.triangles, prn.uv_coords, h, w, prn.resolution_op)
-                    uv_mask = resize(uv_mask, (args.texture_size, args.texture_size), preserve_range = True)
-                    texture = texture*uv_mask[:,:,np.newaxis]
-                write_obj_with_texture(os.path.join(save_folder, name + '.obj'), save_vertices, prn.triangles, texture, prn.uv_coords/prn.resolution_op)#save 3d face with texture(can open with meshlab)
+                    uv_mask = resize(uv_mask, (args.texture_size, args.texture_size), preserve_range=True)
+                    texture = texture * uv_mask[:, :, np.newaxis]
+                write_obj_with_texture(os.path.join(save_folder, name + '.obj'), save_vertices, prn.triangles, texture,
+                                       prn.uv_coords / prn.resolution_op)  # save 3d face with texture(can open with meshlab)
             else:
-                write_obj_with_colors(os.path.join(save_folder, name + '.obj'), save_vertices, prn.triangles, colors) #save 3d face(can open with meshlab)
+                write_obj_with_colors(os.path.join(save_folder, name + '.obj'), save_vertices, prn.triangles,
+                                      colors)  # save 3d face(can open with meshlab)
 
         if args.isDepth:
             depth_image = get_depth_image(vertices, prn.triangles, h, w, True)
             depth = get_depth_image(vertices, prn.triangles, h, w)
             imsave(os.path.join(save_folder, name + '_depth.jpg'), depth_image)
-            sio.savemat(os.path.join(save_folder, name + '_depth.mat'), {'depth':depth})
+            sio.savemat(os.path.join(save_folder, name + '_depth.mat'), {'depth': depth})
 
         if args.isMat:
-            sio.savemat(os.path.join(save_folder, name + '_mesh.mat'), {'vertices': vertices, 'colors': colors, 'triangles': prn.triangles})
+            sio.savemat(os.path.join(save_folder, name + '_mesh.mat'),
+                        {'vertices': vertices, 'colors': colors, 'triangles': prn.triangles})
 
         if args.isKpt or args.isShow:
             # get landmarks
@@ -113,22 +118,26 @@ def main(args):
         if args.isPose or args.isShow:
             # estimate pose
             camera_matrix, pose = estimate_pose(vertices)
-            np.savetxt(os.path.join(save_folder, name + '_pose.txt'), pose) 
-            np.savetxt(os.path.join(save_folder, name + '_camera_matrix.txt'), camera_matrix) 
+            np.savetxt(os.path.join(save_folder, name + '_pose.txt'), pose)
+            np.savetxt(os.path.join(save_folder, name + '_camera_matrix.txt'), camera_matrix)
 
             np.savetxt(os.path.join(save_folder, name + '_pose.txt'), pose)
 
         if args.isShow:
             # ---------- Plot
             image_pose = plot_pose_box(image, camera_matrix, kpt)
-            cv2.imshow('sparse alignment', plot_kpt(image, kpt))
-            cv2.imshow('dense alignment', plot_vertices(image, vertices))
-            cv2.imshow('pose', plot_pose_box(image, camera_matrix, kpt))
-            cv2.waitKey(0)
+            # cv2.imshow('sparse alignment', plot_kpt(image, kpt))
+            # cv2.imshow('dense alignment', plot_vertices(image, vertices))
+            # cv2.imshow('pose', plot_pose_box(image, camera_matrix, kpt))
+            # cv2.waitKey(0)
+            cv2.imwrite(name + '_sparse_alignment.jpg', plot_kpt(image, kpt))
+            cv2.imwrite(name + '_dense_alignment.jpg', plot_vertices(image, vertices))
+            cv2.imwrite(name + '_pose.jpg', plot_pose_box(image, camera_matrix, kpt))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Joint 3D Face Reconstruction and Dense Alignment with Position Map Regression Network')
+    parser = argparse.ArgumentParser(
+        description='Joint 3D Face Reconstruction and Dense Alignment with Position Map Regression Network')
 
     parser.add_argument('-i', '--inputDir', default='TestImages/', type=str,
                         help='path to the input directory, where input images are stored.')
