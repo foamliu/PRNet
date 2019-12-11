cebfeaa38806cb88ee9ded939c18a968bacf6851
Yang Liu <liuyang12@focusmedia.cn>
2019-12-11 15:32:12 +0800

    update

--- demo.py
+++ demo.py
@@ -133,7 +133,7 @@ def main(args):
             # cv2.imshow('dense alignment', plot_vertices(image, vertices))
             # cv2.imshow('pose', plot_pose_box(image, camera_matrix, kpt))
             # cv2.waitKey(0)
-            cv2.imwrite(name + '_image.jpg', image)
+            cv2.imwrite(name + '_image.jpg', img)
             cv2.imwrite(name + '_sparse_alignment.jpg', plot_kpt(img, kpt))
             cv2.imwrite(name + '_dense_alignment.jpg', plot_vertices(img, vertices))
             cv2.imwrite(name + '_pose.jpg', plot_pose_box(img, camera_matrix, kpt))
